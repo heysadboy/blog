e59b737bb869ee767d8c5fd023236c0c13743268
Varun Kumar <heysadboy@gmail.com>
2021-06-05 23:49:45 +0900

    completing blog application

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,24 @@
 import _ from 'lodash';
 import jsonPlaceHolder from '../api/jsonPlaceHolder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) =>  dispatch => _fetchuser(id, dispatch);
-
-// Private function
-const _fetchuser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+}
