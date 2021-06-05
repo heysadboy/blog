bd75c4ad12fd4ed6c9d1d0119f77ebd22d7a9a3b
Varun Kumar <heysadboy@gmail.com>
2021-06-05 22:22:26 +0900

    adding fetch user functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceHolder from '../api/jsonPlaceHolder';
 
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +7,11 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) =>  dispatch => _fetchuser(id, dispatch);
+
+// Private function
+const _fetchuser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
